da0b9bb95261b82c1f80360d65082c37bc03b338
Салимов Артем <salimov.artem@corezoid.com>
2025-07-07 15:58:58 +0300

    fix: improve convctl copy logic for different node_modules paths

--- convctl.sh
+++ convctl.sh
@@ -2,6 +2,10 @@
 
 [ -d .processes ] || mkdir .processes
 if [ ! -f ./convctl ]; then
-  cp ./node_modules/@corezoid/convctl/bin/convctl ./convctl
+  if [ -f ./node_modules/@corezoid/convctl/bin/convctl ]; then
+    cp ./node_modules/@corezoid/convctl/bin/convctl ./convctl
+  elif [ -f ../node_modules/@corezoid/convctl/bin/convctl ]; then
+    cp ../node_modules/@corezoid/convctl/bin/convctl ./convctl
+  fi
 fi
 ./convctl "$@"
